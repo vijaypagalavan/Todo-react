d46c63badb933549c80d56d3514b1ff83eda9b84
unknown <pagalavanps@gmail.com>
2018-10-29 23:39:20 +0530

    alignment fix

--- src/App.js
+++ src/App.js
@@ -32,27 +32,27 @@ class App extends Component {
         this.setState({ todo: [...tempTodo] });
 
     }
-    handleEdit(i,e) {
+    handleEdit(i, e) {
         this.txt.current.value = this.state.todo[i];
         this.state.todo.splice(i, 1);
-        this.setState({  todo: [...this.state.todo],isUpdate: true, tempind: i  });
+        this.setState({ todo: [...this.state.todo], isUpdate: true, tempind: i });
         e.stopPropagation();
     }
     handleUpdate() {
         var tempTodo = [...this.state.todo];
         tempTodo.splice(this.state.tempind, 0, this.txt.current.value);
         this.setState({ todo: [...tempTodo], isUpdate: false, tempind: "" });
-        this.txt.current.value="";
+        this.txt.current.value = "";
     }
-    handleCom(e){
+    handleCom(e) {
         e.stopPropagation();
     }
-    handlePress(e){
-        if (e.key === 'Enter'){
-            if(this.state.isUpdate){
-            this.handleUpdate();
+    handlePress(e) {
+        if (e.key === 'Enter') {
+            if (this.state.isUpdate) {
+                this.handleUpdate();
             }
-            else{
+            else {
                 this.handlechange();
             }
         }
@@ -64,7 +64,7 @@ class App extends Component {
                     <div className="row">
                         <div className="col-md-12">
                             <div className="input-group">
-                                <input className="form-control" placeholder="Enter the list of items" onKeyPress={(e)=> this.handlePress(e)} ref={this.txt}></input>
+                                <input className="form-control" placeholder="Enter the list of items" onKeyPress={(e) => this.handlePress(e)} ref={this.txt}></input>
                                 <span className="input-group-btn">
                                     {this.state.isUpdate ?
                                         <button type="button" className="btn btn-primary" onClick={this.handleUpdate}>Update</button> :
@@ -74,24 +74,24 @@ class App extends Component {
                             </div>
                         </div>
                     </div>
-                <div className="row inp">
-                    <div className="col-md-12">
-                        <ul className="list-group">
-                            {
-                                this.state.todo.map((T, i) => {
-                                    return <li className="list-group-item" onClick={() => !this.state.isUpdate && this.handledel(i)}>
-                                    <input type="checkbox" onClick={(e) => this.handleCom(e)} defaultChecked={false}></input>
-                                        {i + 1 + ". " + T}
-                                        <button className="btn btn-sm itemBtn btn btn-warning" disabled={this.state.isUpdate ? true : false} onClick={(e) => this.handleEdit.call(this, i, e) }>Edit</button>
-                                        <div className="clearfix"></div>
-                                    </li>;
-                                })
-                            }
-                        </ul>
+                    <div className="row inp">
+                        <div className="col-md-12">
+                            <ul className="list-group">
+                                {
+                                    this.state.todo.map((T, i) => {
+                                        return <li className="list-group-item" onClick={() => !this.state.isUpdate && this.handledel(i)}>
+                                            <input type="checkbox" onClick={(e) => this.handleCom(e)} defaultChecked={false}></input>
+                                            {i + 1 + ". " + T}
+                                            <button className="btn btn-sm itemBtn btn btn-warning" disabled={this.state.isUpdate ? true : false} onClick={(e) => this.handleEdit.call(this, i, e)}>Edit</button>
+                                            <div className="clearfix"></div>
+                                        </li>;
+                                    })
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-         </div>
         );
     }
 }
